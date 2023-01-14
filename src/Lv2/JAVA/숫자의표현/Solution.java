5ab60c4814b058690e6cef2f18bd2691ef0ee080
DESKTOP-NT05U6L\Yeop <tmdduq5498@gmail.com>
2023-01-15 00:00:10 +0900

    Lv2.Java 추가

---
+++ src/Lv2/JAVA/숫자의표현/Solution.java
@@ -0,0 +1,17 @@
+package Lv2.JAVA.숫자의표현;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 1; //최소값 = 자기 자신
+
+        for(int i = 1; i <= n / 2; i++) {
+            int sum = i;
+            for(int j = i + 1; sum < n; j++) {
+                sum += j;
+            }
+            if(sum == n) answer++;
+        }
+
+        return answer;
+    }
+}
